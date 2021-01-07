197b9ea9547186d8f640307917eaa3e868cf2102
russeljeanne <russeljeannepinlac@gmail.com>
2021-01-08 02:54:18 +0800

    updated

--- Mobile_Voting_app/app/src/main/java/com/example/mobile_voting_app/MainActivity.java
+++ Mobile_Voting_app/app/src/main/java/com/example/mobile_voting_app/MainActivity.java
@@ -4,19 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //button variables
-    Button signup_btn;
+    Button signup_btn,login_btn;
+    //edit text variables
+    EditText username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login_btn = findViewById(R.id.login_btn);
         signup_btn = findViewById(R.id.singup_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.length()==0 && password.length()==0) {
+                    username.setError("Enter username");
+                    password.setError("Enter password");
+                }
+                else if (username.length()==0) {
+                    username.setError("Enter username");
+                }
+                else if (password.length()==0) {
+                    password.setError("Enter password");
+                }
+            }
+        });
+
         signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- Mobile_Voting_app/app/src/main/java/com/example/mobile_voting_app/signup.java
+++ Mobile_Voting_app/app/src/main/java/com/example/mobile_voting_app/signup.java
@@ -13,15 +13,14 @@ import android.widget.Toast;
 public class signup extends AppCompatActivity {
 
     TextView age_et,password_et,confirmpass_et;
-    Button signup_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-    age_et = findViewById(R.id.age_et);
-    password_et = findViewById(R.id.password_et);
-    confirmpass_et = findViewById(R.id.confirmpass_et);
+        age_et = findViewById(R.id.age_et);
+        password_et = findViewById(R.id.password_et);
+        confirmpass_et = findViewById(R.id.confirmpass_et);
     }
 
     public void signup_btn(View view) {
@@ -31,7 +30,7 @@ public class signup extends AppCompatActivity {
         if (age < 18) {
             Toast.makeText(this,"Age below 18 is not allowed.",Toast.LENGTH_SHORT).show();
         }
-        if (!confirmpass.equals(pass)) {
+        else if (!confirmpass.equals(pass)) {
             Toast.makeText(this, "Password did not match.",Toast.LENGTH_SHORT).show();
         }
     }
